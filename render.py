6c432e9efd08b4fe3a9fcd1f1d09a1ac654bbe14
Michael Kling <mwkling@gmail.com>
2019-11-29 16:13:11 -0500

    Add ability to generate animated gifs.  Update necessary libraries.

--- render.py
+++ render.py
@@ -2,6 +2,10 @@ import pygame
 import sys
 import argparse
 import random
+import shutil
+import os
+import imageio
+import pygifsicle
 
 from strategies.separation import Separation
 from strategies.top_down import TopDown
@@ -14,6 +18,7 @@ BLACK = (0, 0, 0)
 BLUE  = (0, 0, 255)
 RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
+FRAME_PATH = "frames/frame_{0}.png"
 
 
 def random_rect():
@@ -36,10 +41,12 @@ def random_rect():
 
 
 class Renderer:
-    def __init__(self, rectangles, framerate, strategy, input_rectangles, output_rectangles):
+    def __init__(self, rectangles, framerate, strategy, input_rectangles, output_rectangles,
+                 output_gif):
         self.framerate = framerate
         self.input_rectangles = input_rectangles
         self.output_rectangles = output_rectangles
+        self.output_gif = output_gif
 
         C = getattr(sys.modules[__name__], strategy)
 
@@ -51,6 +58,10 @@ class Renderer:
         if output_rectangles: Rectangle.to_csv(start_rectangles, output_rectangles)
 
         self.stepper = C(start_rectangles)
+
+        if output_gif:
+            shutil.rmtree("frames", ignore_errors=True)
+            os.mkdir("frames")
 
         pygame.init()
         size = width, height = 1000, 1000
@@ -66,25 +77,54 @@ class Renderer:
         pygame.draw.line(self.screen, RED, (rect.midx, rect.midy),
                          (rect.original_midx, rect.original_midy))
 
+    def draw_rectangles(self):
+        # Draw rectangles themselves
+        for r in self.stepper.rectangles:
+            self.draw_rectangle(r)
+
+        # Draw overlaping sections of the rectangles in green
+        for r in Rectangle.overlap_rectangles(self.stepper.rectangles):
+            self.draw_rectangle(r, fill=GREEN)
+
+    def save_frame(self, i):
+        pygame.image.save(self.screen, FRAME_PATH.format(i))
+
+    def save_gif(self, frames):
+        # Last frame wasn't saved previously
+        self.save_frame(frames)
+
+        with imageio.get_writer(self.output_gif, mode="I") as writer:
+            for i in range(frames):
+                writer.append_data(imageio.imread(FRAME_PATH.format(i)))
+            # Add 10 copies of final frame at the end
+            for _ in range(10):
+                writer.append_data(imageio.imread(FRAME_PATH.format(frames)))
+        pygifsicle.optimize(self.output_gif)
+
     def run(self):
         clock = pygame.time.Clock()
         done = False
+        frame = 0
 
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    if self.output_gif:
+                        self.save_gif(frame)
+
                     pygame.quit()
                     sys.exit()
 
             self.screen.fill(WHITE)
-            for r in self.stepper.rectangles:
-                self.draw_rectangle(r)
-
-            for r in Rectangle.overlap_rectangles(self.stepper.rectangles):
-                self.draw_rectangle(r, fill=GREEN)
+            self.draw_rectangles()
 
             if not done:
                 self.stepper.step()
+
+                if self.output_gif:
+                    self.save_frame(frame)
+                    frame += 1
+
                 if not Rectangle.has_overlaps(self.stepper.rectangles):
                     print("No remaining overlaps!")
                     print("Total movement: ", Rectangle.total_movement(self.stepper.rectangles))
@@ -101,9 +141,10 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--strategy", default="Separation")
     parser.add_argument("-i", "--input_rectangles", help="CSV with saved rectangle configuration")
     parser.add_argument("-o", "--output_rectangles", help="CSV to write generated rectangles to")
+    parser.add_argument("-g", "--output_gif", help="GIF file to render output to")
 
     args = parser.parse_args()
 
     renderer = Renderer(args.rectangles, args.framerate, args.strategy,
-                        args.input_rectangles, args.output_rectangles)
+                        args.input_rectangles, args.output_rectangles, args.output_gif)
     renderer.run()
